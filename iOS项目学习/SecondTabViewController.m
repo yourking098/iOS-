f84ce0a4e876af923f55919cdb40b77a4d8af27f
yourking098 <465918193@qq.com>
2016-03-11 15:33:14 +0800

    测试UILabel首行缩进
    测试UILabel首行缩进

--- iOS项目学习/SecondTabViewController.m
+++ iOS项目学习/SecondTabViewController.m
@@ -8,7 +8,12 @@
 
 #import "SecondTabViewController.h"
 
-@interface SecondTabViewController ()
+@interface SecondTabViewController () {
+    NSString *strContent;
+    
+}
+@property(strong,nonatomic) UILabel *usernameLabel;
+@property(strong,nonatomic) UILabel *contentLabel;
 
 @end
 
@@ -17,9 +22,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.usernameLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 100, 200, 50)];
+    self.usernameLabel.text = @"用户名Jordan CZ: ";
+    self.usernameLabel.adjustsFontSizeToFitWidth = YES ;
+    [self.usernameLabel sizeToFit];
+    
+    strContent = @"首行缩进根据用户昵称自动调整 间隔可自定根据需求随意改变。。。。";
+    self.contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 150, 320, 150)];
+    self.contentLabel.text = strContent;
+    self.contentLabel.layer.borderColor = [[UIColor redColor] CGColor];
+    self.contentLabel.layer.borderWidth = 1;
+    self.contentLabel.numberOfLines = 0;
+    self.contentLabel.adjustsFontSizeToFitWidth=YES;
+    self.contentLabel.adjustsLetterSpacingToFitWidth=YES;
+    
+    [self.view addSubview:self.contentLabel];
+    [self.view addSubview:self.usernameLabel];
+    
+    [self resetContent];
 }
 
+//自适应计算间距
+- (void)resetContent{
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:strContent];
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.alignment = NSTextAlignmentLeft ;
+    paragraphStyle.maximumLineHeight = 60 ;  //最大的行高
+    paragraphStyle.lineSpacing = 5 ;  //行自定义行高度
+    [paragraphStyle setFirstLineHeadIndent:50]; //首行缩进 根据用户昵称宽度在加5个像素
+    [attributedString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, [self.contentLabel.text length])];
+    self.contentLabel.attributedText = attributedString;
+    [self.contentLabel sizeToFit];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
